18ade0cf38b3d025fe992811b886e88761aa94c4
iuun <280656495@qq.com>
2018-01-13 00:24:20 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
     	return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+    	return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|unique:users|max:255',
+    		'password' => 'required|confirmed|min:6'
+    	]);
+
+    	$user = User::create([
+    		'name' => $request->name,
+    		'email' => $request->email,
+    		'password' => bcrypt($request->password),
+    	]);
+
+    	session()->flash('success', '欢迎，您将在这里开启一段新的旅程');
+    	return redirect()->route('users.show', [$user]); 
+    	//此时 $user 是 User 模型对象的实例.route() 方法会自动获取 Model 的主键，这里就是id, $user 等同于 $user->id
     }
 }
--- laravel笔记.php
+++ laravel笔记.php
@@ -8,3 +8,8 @@ namespace 代表的是 命名空间
 view文件中重复的代码都放在views/layout/文件下
 
 yarn install --no-bin-links【安装yarn】
+
+ 『隐性路由模型绑定』 http://d.laravel-china.org/docs/5.5/routing#%E9%9A%90%E5%BC%8F%E7%BB%91%E5%AE%9A
+
+路由声明时必须使用 Eloquent 模型的单数小写格式来作为路由片段参数，User 对应 {user}
+
